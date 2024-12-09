c3ea952c52fb677ca8b52b2b45cbe877c1d9e0a5
SilvesterSpath <spath.sz@gmail.com>
2024-12-09 10:02:16 +0100

    SearchScreeen init

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
+
+const AppNavigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+  },
+  {
+    initialRouteName: 'Search',
+  }
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SearchScreen />
+      <StatusBar style='auto' />
     </View>
   );
 }

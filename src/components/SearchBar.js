e4888cb52967136b5223225b17f39dcc81f18f1a
SilvesterSpath <spath.sz@gmail.com>
2024-12-11 05:54:30 +0100

    SearchBar style

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,28 +4,32 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <AntDesign name='search1' size={24} color='black' style={styles.icon} />
+    <View style={styles.backgroundStyle}>
+      <AntDesign name='search1' color='black' style={styles.icon} />
       <TextInput placeholder='Search' style={styles.input} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 13,
+    marginHorizontal: 15,
+    marginTop: 10,
     flexDirection: 'row',
-    alignItems: 'center', // Vertically center icon and input
   },
   icon: {
-    marginHorizontal: 10, // Add space around the icon
+    marginHorizontal: 15, // Add space around the icon
+    fontSize: 35,
+    alignSelf: 'center', // Vertically center the icon
   },
   input: {
     flex: 1, // Allow input to take remaining space
-    marginRight: 10, // Prevent text from touching the edge
+    fontSize: 18,
+    /*     borderColor: 'black',
+    borderWidth: 1, */
   },
 });
 

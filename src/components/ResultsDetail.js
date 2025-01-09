339ac8afdd4b8dd2d14ab0fa93fbc50c248f87fc
SilvesterSpath <spath.sz@gmail.com>
2025-01-09 05:43:11 +0100

    Image

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -4,11 +4,22 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 const ResultsDetail = ({ result }) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image source={{ uri: result.image_url }} style={styles.image} />
+      <Text style={styles.name}>{result.name}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+    marginBottom: 5,
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+});
 
 export default ResultsDetail;

9c6a32b723386b76e29dc5e0194ef2d04727380b
SilvesterSpath <spath.sz@gmail.com>
2025-01-15 09:18:01 +0100

    data renov

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -6,12 +6,21 @@ const imageMapping = {
   burger1: require('../data/images/burger1.jpg'),
   burger2: require('../data/images/burger2.jpg'),
   burger3: require('../data/images/burger3.jpg'),
+  burger4: require('../data/images/burger4.jpg'),
+  burger5: require('../data/images/burger5.jpg'),
+  burger6: require('../data/images/burger6.jpg'),
   pasta1: require('../data/images/pasta1.jpg'),
   pasta2: require('../data/images/pasta2.jpg'),
+  pasta4: require('../data/images/pasta4.jpg'),
   pasta3: require('../data/images/pasta3.jpg'),
+  pasta5: require('../data/images/pasta5.jpg'),
+  pasta6: require('../data/images/pasta6.jpg'),
   pizza1: require('../data/images/pizza1.jpg'),
   pizza2: require('../data/images/pizza2.jpg'),
   pizza3: require('../data/images/pizza3.jpg'),
+  pizza4: require('../data/images/pizza4.jpg'),
+  pizza5: require('../data/images/pizza5.jpg'),
+  pizza6: require('../data/images/pizza6.jpg'),
 };
 
 const ResultsDetail = ({ result }) => {

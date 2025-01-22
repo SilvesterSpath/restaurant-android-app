c73263be208caf709cb5b3b9d6be80012d847c39
SilvesterSpath <spath.sz@gmail.com>
2025-01-22 07:37:12 +0100

    ResultsShowScreen init

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+
+const ResultsShowScreen = () => {
+  return <div>ResultsShowScreen</div>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;

9f8d8cb460579c70e8665df3bfb42a6bf3022249
SilvesterSpath <spath.sz@gmail.com>
2025-01-22 05:41:36 +0100

    fix scroll

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,49 +1,42 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import SearchBar from '../components/SearchBar';
-import ResultsCard from '../components/ResutsCard';
-import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 import data from '../data/data.json';
 
-/* import Constants from 'expo-constants';
-
-const extra = Constants.expoConfig?.extra;
-
-const API_KEY = extra.API_KEY || 'default_api_key';
-const CLIENT_ID = extra.CLIENT_ID || 'default_client_id';
-
-console.log('API_KEY:', API_KEY);
-console.log('CLIENT_ID:', CLIENT_ID); */
-
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, error, searchApi] = useResults();
 
   const filterResultsByPrice = (price) => {
-    // price === '$' || '$$' || '$$$'
-    return data.filter((item) => {
-      return item.price === price;
-    });
+    return data.filter((item) => item.price === price);
   };
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
       <View style={styles.searchBarContainer}>
         <SearchBar
           style={styles.searchBar}
           term={term}
           onTermChange={setTerm}
-          onTermSubmit={() => searchApi(term)}
+          onTermSubmit={() => console.log('Search API Call')}
         />
       </View>
 
-      {error && <Text>{error}</Text>}
-      <Text style={{ marginLeft: 10 }}>
-        {' '}
+      <Text style={styles.resultsText}>
         We have found {data.length} results
       </Text>
-      <ScrollView>
+
+      <ScrollView contentContainerStyle={styles.scrollContent}>
         <ResultsList
           results={filterResultsByPrice('$')}
           title='CostEffective'
@@ -55,13 +48,24 @@ const SearchScreen = () => {
           title='Extravagant'
         />
       </ScrollView>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Take up the full screen
+  },
   searchBarContainer: {
-    /*     marginLeft: 10, */
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  resultsText: {
+    marginLeft: 10,
+    marginBottom: 5,
+  },
+  scrollContent: {
+    paddingBottom: 20, // Ensure there's padding at the bottom for visibility
   },
 });
 

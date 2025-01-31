ebf83ec2f69e1d912325a48828c30e85c415bc22
SilvesterSpath <spath.sz@gmail.com>
2025-01-31 06:26:44 +0100

    showing one business

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import data from '../data/data.json';
+import ResultsCard from '../components/ResutsCard';
+import ResultsDetail from '../components/ResultsDetail';
 
 const ResultsShowScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
 
+  const filterResultsById = (id) => {
+    return data.filter((item) => item.id === id);
+  };
+
+  const result = filterResultsById(id)[0];
+  console.log('ResultsShow result:', result);
+
   return (
     <View>
       <Text>ResultsShowScreen</Text>
+      <Text>{id}</Text>
+      <ResultsDetail result={result} />
     </View>
   );
 };
